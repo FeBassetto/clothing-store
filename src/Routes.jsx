0894152cca0be279f6a2d615d501fc32073e86ad
Felipe Bassetto <felipebtu9@gmail.com>
2022-06-03 10:03:28 -0300

    Creating Container

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -2,15 +2,20 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Header from './components/Header/Header';
+import Footer from "./components/Footer/Footer";
+import Container from "./components/Container/Container";
 
 
 const Routing = () => {
     return (
         <Router>
             <Header />
-            <Routes>
-                <Route exact path="/" element={<Home />} />
-            </Routes>
+            <Container>
+                <Routes>
+                    <Route exact path="/" element={<Home />} />
+                </Routes>
+            </Container>
+            <Footer />
         </Router>
     )
 }
