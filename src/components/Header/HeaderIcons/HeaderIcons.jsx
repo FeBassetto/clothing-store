d5e9e42f7318254f4e484f1d28c03e641a43e3a2
Felipe Bassetto <felipebtu9@gmail.com>
2022-06-03 08:51:10 -0300

    Adding link to header icons

--- src/components/Header/HeaderIcons/HeaderIcons.jsx
+++ src/components/Header/HeaderIcons/HeaderIcons.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import style from './HeaderIcons.module.css'
 import { GrCart } from 'react-icons/gr'
 import { AiOutlineHeart, AiOutlineUser } from 'react-icons/ai'
+import { Link } from "react-router-dom";
 
 const HeaderIcons = () => {
     return (
         <div className={style.icons} >
-            <GrCart />
-            <AiOutlineHeart />
-            <AiOutlineUser />
+            <Link to='cart' ><GrCart /></Link>
+            <Link to='favorites' ><AiOutlineHeart /></Link>
+            <Link to='user' ><AiOutlineUser /></Link>
         </div>
     )
 }
